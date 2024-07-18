b48b1c7b5c29a5b7c0c56d12dd62ff1f56a0e8ee
balashankar317 <balashankar9885@gmail.com>
2024-07-18 15:41:37 +0530

    1st

---
+++ src/main/java/com/bb/BootBasicApplication.java
@@ -0,0 +1,12 @@
+package com.bb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class BootBasicApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(BootBasicApplication.class, args);
+    }
+}
